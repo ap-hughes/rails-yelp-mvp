c1999cdfb8fe0846aa2b6c40be2bfeb5c250cba1
Alex Hughes <aphughes@talk21.com>
2018-02-21 12:39:17 +0000

    Models and Controllers created

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,10 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+Restaurant.create(name: "Pizza East", address: "Shoreditch", phone_number: "00 44 208 330 0022", category: "italian")
+Restaurant.create(name: "Cafe Rouge", address: "Highgate", phone_number: "00 44 208 440 1155", category: "french")
+Restaurant.create(name: "Cote", address: "Hampstead", phone_number: "00 44 208 670 2555", category: "french")
+Restaurant.create(name: "Bone Daddies", address: "Soho", phone_number: "00 44 208 754 2859", category: "japanese")
+Restaurant.create(name: "Pizza Pilgrims", address: "Carnaby Street", phone_number: "00 44 208 748 4567", category: "italian")
